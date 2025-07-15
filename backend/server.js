abc2eb8d844b2c75dfba24a54921abaf68f9678c
CristiannGuerra <camguerra93@gmail.com>
2025-07-14 22:59:31 -0300

    backend-cors-fixed

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(helmet());
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors())
 
 app.use(express.json(
   {limit: '50mb'}
